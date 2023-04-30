ebaa4a5123c3e990ee6352e690bb2aefb467a0fb
Sonlowami <uwilowami64@outlook.com>
2023-04-30 18:19:17 +0200

    Generate a 404 error in json

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module contain a flask application that deals with APIs"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -10,7 +10,14 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close(exception):
+    """Call storage.close to close a session and get another"""
     storage.close()
+
+@app.errorhandler(404)
+def handle404(error):
+    """Handle error, 404"""
+    return make_response(jsonify({ "error": "Not found" }), 404)
+
 
 
 if __name__ == '__main__':
